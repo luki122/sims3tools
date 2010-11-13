f862ad781daad671ad9f01fe6a9baaff4e53df11
pljones <pljones@da88ef80-c9c6-43c1-8b9e-c1dffb33cf72>
2010-11-13 13:53:47 +0000

    Re-organise repository: move 's3pe OBJK Resource Editor' to 'trunk/s3pe OBJK Resource Editor'

---
+++ trunk/s3pe OBJK Resource Editor/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("s3pe OBJK Resource Editor")]
+[assembly: AssemblyDescription("A tool to make editing ObjKey resources easier")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Peter L Jones")]
+[assembly: AssemblyProduct("s3pe OBJK Resource Editor")]
+[assembly: AssemblyCopyright("Copyright © 2009  Peter L Jones  Released under the GPL 3.0")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("817b6eca-5000-4555-9d20-d301e21b8032")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+//[assembly: AssemblyVersion("1.0.0.0")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
