42a21d6d7a9f8d2007150694083d1ca6ce7f1990
pljones <pljones@da88ef80-c9c6-43c1-8b9e-c1dffb33cf72>
2010-11-13 13:55:41 +0000

    Re-organise repository: move 's3pe VPXY Resource Editor' to 'trunk/s3pe VPXY Resource Editor'

---
+++ s3pe VPXY Resource Editor/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("s3pe VPXY Resource Editor")]
+[assembly: AssemblyDescription("A tool to make editing Visual Proxy resources easier")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Peter L Jones")]
+[assembly: AssemblyProduct("s3pe VPXY Resource Editor")]
+[assembly: AssemblyCopyright("Copyright © 2009  Peter L Jones  Released under the GPL 3.0")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7ee12219-527f-43d2-94a3-2085446c2d54")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+//[assembly: AssemblyVersion("1.0.0.0")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
