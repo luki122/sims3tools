f3ac0e19ba1594e4877ee07fcc118bdc2da53fe8
pljones <pljones@da88ef80-c9c6-43c1-8b9e-c1dffb33cf72>
2010-11-28 10:08:13 +0000

    New S3SA DLL Export/Import helper.

---
+++ s3pe Helpers/S3SA DLL ExpImp/Import.cs
@@ -0,0 +1,73 @@
+﻿/***************************************************************************
+ *  Copyright (C) 2010 by Peter L Jones                                    *
+ *  pljones@users.sf.net                                                   *
+ *                                                                         *
+ *  This file is part of the Sims 3 Package Interface (s3pi)               *
+ *                                                                         *
+ *  s3pi is free software: you can redistribute it and/or modify           *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  s3pi is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with s3pi.  If not, see <http://www.gnu.org/licenses/>.          *
+ ***************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using s3pi.Interfaces;
+using ScriptResource;
+
+namespace S3SA_DLL_ExpImp
+{
+    public partial class Import : Form, s3pi.Helpers.IRunHelper
+    {
+        public Import()
+        {
+            InitializeComponent();
+        }
+
+        ScriptResource.ScriptResource s3sa;
+        public Import(Stream s)
+            : this()
+        {
+            s3sa = new ScriptResource.ScriptResource(0, s);
+            Application.DoEvents();
+        }
+
+        byte[] result = null;
+        public byte[] Result { get { return result; } }
+
+        private void Import_Load(object sender, EventArgs e)
+        {
+            Application.DoEvents();
+
+            ofdImport.FileName = Program.getAssemblyName(s3sa);
+            DialogResult dr = ofdImport.ShowDialog();
+            if (dr != DialogResult.OK)
+            {
+                Environment.ExitCode = 1;
+            }
+
+            FileStream fs = new FileStream(ofdImport.FileName, FileMode.Open, FileAccess.Read);
+            BinaryReader br = new BinaryReader(fs);
+            s3sa.Assembly = br;
+            br.Close();
+            fs.Close();
+
+            result = (byte[])s3sa.AsBytes.Clone();
+
+            Application.DoEvents();
+            Environment.ExitCode = 0;
+            this.Close();
+        }
+    }
+}
---
+++ s3pe Helpers/S3SA DLL ExpImp/Program.cs
@@ -0,0 +1,82 @@
+﻿/***************************************************************************
+ *  Copyright (C) 2010 by Peter L Jones                                    *
+ *  pljones@users.sf.net                                                   *
+ *                                                                         *
+ *  This file is part of the Sims 3 Package Interface (s3pi)               *
+ *                                                                         *
+ *  s3pi is free software: you can redistribute it and/or modify           *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  s3pi is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with s3pi.  If not, see <http://www.gnu.org/licenses/>.          *
+ ***************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace S3SA_DLL_ExpImp
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static int Main(params string[] args)
+        {
+            List<string> largs = new List<string>(args);
+
+            bool export = largs.Contains("/export");
+            if (export) largs.Remove("/export");
+
+            bool import = largs.Contains("/import");
+            if (import) largs.Remove("/import");
+
+            if ((!export && !import) || (export && import))
+            {
+                CopyableMessageBox.Show("Invalid command line.", Application.ProductName, CopyableMessageBoxButtons.OK, CopyableMessageBoxIcon.Error);
+                Environment.Exit(1);
+            }
+
+            args = largs.ToArray();
+
+#if DEBUG
+            if (args.Length == 0)
+            {
+                FileStream fs = new FileStream(Application.ExecutablePath, FileMode.Open, FileAccess.Read);
+                BinaryReader br = new BinaryReader(fs);
+                Clipboard.SetData(DataFormats.Serializable, new ScriptResource.ScriptResource(0, null) { Assembly = br }.Stream);
+                br.Close();
+                fs.Close();
+            }
+#endif
+
+            return s3pi.Helpers.RunHelper.Run(export ? typeof(Export) : typeof(Import), args);
+        }
+
+        public static string getAssemblyName(ScriptResource.ScriptResource s3sa)
+        {
+            try
+            {
+                byte[] data = new byte[s3sa.Assembly.BaseStream.Length];
+                s3sa.Assembly.BaseStream.Read(data, 0, data.Length);
+                Assembly assy = Assembly.Load(data);
+
+                return assy.FullName.Split(',')[0] + ".dll";
+            }
+            catch
+            {
+            }
+            return "*.dll";
+        }
+    }
+}
