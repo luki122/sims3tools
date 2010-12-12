104623eda1a3dc621a0624741982e3e2d9b1c38a
pljones <pljones@da88ef80-c9c6-43c1-8b9e-c1dffb33cf72>
2010-12-12 10:23:08 +0000

    Change default FormWindowState to Minimized (causing a form layout reset).

--- s3pe/Properties/Settings.Designer.cs
+++ s3pe/Properties/Settings.Designer.cs
@@ -96,7 +96,7 @@ namespace S3PIDemoFE.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        [global::System.Configuration.DefaultSettingValueAttribute("1")]
         public int FormWindowState {
             get {
                 return ((int)(this["FormWindowState"]));
