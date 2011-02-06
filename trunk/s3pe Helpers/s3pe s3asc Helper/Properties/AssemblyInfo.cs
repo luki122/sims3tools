8bdde88888528525a585190a205f8e071d610787
pljones <pljones@da88ef80-c9c6-43c1-8b9e-c1dffb33cf72>
2011-02-06 19:00:26 +0000

    Hopefully fix error with Properties folder.

---
+++ trunk/s3pe Helpers/s3pe s3asc Helper/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("s3pe s3asc Helper")]
+[assembly: AssemblyDescription("s3pe's own s3asc Export/Import tool")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Peter L Jones")]
+[assembly: AssemblyProduct("S3SA DLL ExpImp")]
+[assembly: AssemblyCopyright("Copyright © 2011  Peter L Jones  Released under the GPL 3.0")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3aaece20-3049-4435-a379-89e6c85d122c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+//[assembly: AssemblyVersion("1.0.0.0")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
