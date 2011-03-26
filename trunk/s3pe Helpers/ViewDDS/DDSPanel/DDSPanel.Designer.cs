fc272d98d61eff9e8e197fd71937efaa9598b14b
pljones <pljones@da88ef80-c9c6-43c1-8b9e-c1dffb33cf72>
2011-03-26 14:22:25 +0000

    Split Helper app code from DDS viewer code, making a re-useable component from the latter.

---
+++ trunk/s3pe Helpers/ViewDDS/DDSPanel/DDSPanel.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace DDSPanel
+{
+    partial class DDSPanel
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ckbR = new System.Windows.Forms.CheckBox();
+            this.ckbG = new System.Windows.Forms.CheckBox();
+            this.ckbB = new System.Windows.Forms.CheckBox();
+            this.ckbA = new System.Windows.Forms.CheckBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(329, 200);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(3, 32);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(323, 165);
+            this.panel1.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(128, 128);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.flowLayoutPanel1.Controls.Add(this.label1);
+            this.flowLayoutPanel1.Controls.Add(this.ckbR);
+            this.flowLayoutPanel1.Controls.Add(this.ckbG);
+            this.flowLayoutPanel1.Controls.Add(this.ckbB);
+            this.flowLayoutPanel1.Controls.Add(this.ckbA);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(320, 23);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Enable channels:";
+            // 
+            // ckbR
+            // 
+            this.ckbR.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ckbR.AutoSize = true;
+            this.ckbR.Checked = true;
+            this.ckbR.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.ckbR.Location = new System.Drawing.Point(98, 3);
+            this.ckbR.Name = "ckbR";
+            this.ckbR.Size = new System.Drawing.Size(46, 17);
+            this.ckbR.TabIndex = 0;
+            this.ckbR.Text = "Red";
+            this.ckbR.UseVisualStyleBackColor = true;
+            this.ckbR.CheckedChanged += new System.EventHandler(this.ckb_CheckedChanged);
+            // 
+            // ckbG
+            // 
+            this.ckbG.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ckbG.AutoSize = true;
+            this.ckbG.Checked = true;
+            this.ckbG.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.ckbG.Location = new System.Drawing.Point(150, 3);
+            this.ckbG.Name = "ckbG";
+            this.ckbG.Size = new System.Drawing.Size(55, 17);
+            this.ckbG.TabIndex = 1;
+            this.ckbG.Text = "Green";
+            this.ckbG.UseVisualStyleBackColor = true;
+            this.ckbG.CheckedChanged += new System.EventHandler(this.ckb_CheckedChanged);
+            // 
+            // ckbB
+            // 
+            this.ckbB.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ckbB.AutoSize = true;
+            this.ckbB.Checked = true;
+            this.ckbB.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.ckbB.Location = new System.Drawing.Point(211, 3);
+            this.ckbB.Name = "ckbB";
+            this.ckbB.Size = new System.Drawing.Size(47, 17);
+            this.ckbB.TabIndex = 2;
+            this.ckbB.Text = "Blue";
+            this.ckbB.UseVisualStyleBackColor = true;
+            this.ckbB.CheckedChanged += new System.EventHandler(this.ckb_CheckedChanged);
+            // 
+            // ckbA
+            // 
+            this.ckbA.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ckbA.AutoSize = true;
+            this.ckbA.Checked = true;
+            this.ckbA.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.ckbA.Location = new System.Drawing.Point(264, 3);
+            this.ckbA.Name = "ckbA";
+            this.ckbA.Size = new System.Drawing.Size(53, 17);
+            this.ckbA.TabIndex = 3;
+            this.ckbA.Text = "Alpha";
+            this.ckbA.UseVisualStyleBackColor = true;
+            this.ckbA.CheckedChanged += new System.EventHandler(this.ckb_CheckedChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // DDSPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "DDSPanel";
+            this.Size = new System.Drawing.Size(329, 200);
+            this.Resize += new System.EventHandler(this.DDSPanel_Resize);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox ckbR;
+        private System.Windows.Forms.CheckBox ckbG;
+        private System.Windows.Forms.CheckBox ckbB;
+        private System.Windows.Forms.CheckBox ckbA;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ trunk/s3pe Helpers/ViewDDS/DDSPanel/DDSPanel.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+using DdsFileTypePlugin;
+using System.IO;
+
+namespace DDSPanel
+{
+    public partial class DDSPanel : UserControl
+    {
+
+        public DDSPanel()
+        {
+            InitializeComponent();
+        }
+
+        bool fit = false;
+        [DefaultValue(false), Description("Set to true to have the image resize to the control bounds")]
+        public bool Fit { get { return fit; } set { fit = value; pictureBox1.Image = doResize(); OnChanged(FitChanged); } }
+
+        Size maxSize = new Size(0, 0);
+        [Description("Set non-zero bounds to constrain the image size")]
+        public Size MaxSize { get { return maxSize; } set { maxSize = value; pictureBox1.Image = doResize(); OnChanged(MaxSizeChanged); } }
+
+        [DefaultValue(true), Description("Display channel 1 as Red")]
+        public bool Channel1 { get { return ckbR.Checked; } set { ckbR.Checked = value; OnChanged(Channel1Changed); } }
+        [DefaultValue(true), Description("Display channel 2 as Green")]
+        public bool Channel2 { get { return ckbG.Checked; } set { ckbG.Checked = value; OnChanged(Channel2Changed); } }
+        [DefaultValue(true), Description("Display channel 3 as Blue")]
+        public bool Channel3 { get { return ckbB.Checked; } set { ckbB.Checked = value; OnChanged(Channel3Changed); } }
+        [DefaultValue(true), Description("Display channel 4 as Alpha")]
+        public bool Channel4 { get { return ckbA.Checked; } set { ckbA.Checked = value; OnChanged(Channel4Changed); } }
+
+        [DefaultValue(true), Description("Show the Channel Selection check boxes")]
+        public bool ShowChannelSelector { get { return flowLayoutPanel1.Visible; } set { flowLayoutPanel1.Visible = value; pictureBox1.Image = doResize(); OnChanged(ShowChannelSelectorChanged); } }
+
+        [Description("Raised to indicate Fit value changed")]
+        public event EventHandler FitChanged;
+        [Description("Raised to indicate MaxSize value changed")]
+        public event EventHandler MaxSizeChanged;
+        [Description("Raised to indicate Channel1 value changed")]
+        public event EventHandler Channel1Changed;
+        [Description("Raised to indicate Channel2 value changed")]
+        public event EventHandler Channel2Changed;
+        [Description("Raised to indicate Channel3 value changed")]
+        public event EventHandler Channel3Changed;
+        [Description("Raised to indicate Channel4 value changed")]
+        public event EventHandler Channel4Changed;
+        [Description("Raised to indicate ShowChannelSelector value changed")]
+        public event EventHandler ShowChannelSelectorChanged;
+
+        protected void OnChanged(EventHandler h) { if (h != null) h(this, EventArgs.Empty); }
+
+        DdsFile ddsFile = new DdsFile();
+        bool loaded = false;
+        /// <summary>
+        /// Load a DDS image from a <see cref="System.IO.Stream"/>
+        /// </summary>
+        /// <param name="stream">The <see cref="System.IO.Stream"/> containing the DDS image to display</param>
+        public void DDSLoad(Stream stream)
+        {
+            try
+            {
+                this.Enabled = false;
+                Application.UseWaitCursor = true;
+                ddsFile.Load(stream);
+                loaded = true;
+            }
+            finally { this.Enabled = true; Application.UseWaitCursor = false; }
+            ckb_CheckedChanged(null, null);
+        }
+
+        DateTime now = DateTime.UtcNow;
+
+        Image image;
+        private void ckb_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!loaded) return;
+
+            try
+            {
+                this.Enabled = false;
+                Application.UseWaitCursor = true;
+                image = ddsFile.Image(ckbR.Checked, ckbG.Checked, ckbB.Checked, ckbA.Checked);
+                pictureBox1.Image = doResize();
+            }
+            finally { this.Enabled = true; Application.UseWaitCursor = false; }
+
+            if (sender != null)
+            {
+                if (sender == ckbR) Channel1 = ckbR.Checked;
+                if (sender == ckbG) Channel2 = ckbG.Checked;
+                if (sender == ckbB) Channel3 = ckbB.Checked;
+                if (sender == ckbA) Channel4 = ckbA.Checked;
+            }
+        }
+
+        Image doResize()
+        {
+            if (!loaded) return null;
+
+            Image targetImage = image;
+            Size targetSize = targetImage.Size;
+
+            if (Fit) targetSize = ScaleToFit(targetSize, panel1.ClientSize);
+
+            targetSize = ScaleToFit(targetSize, maxSize);
+
+            pictureBox1.Size = targetSize;
+
+            if (targetImage.Size.Width > targetSize.Width || targetImage.Size.Height > targetSize.Height)
+                targetImage = targetImage.GetThumbnailImage(
+                    targetImage.Size.Width > targetSize.Width ? targetSize.Width : targetImage.Size.Width,
+                    targetImage.Size.Height > targetSize.Height ? targetSize.Height : targetImage.Size.Height,
+                    gtAbort, System.IntPtr.Zero);
+
+            return targetImage;
+        }
+        static bool gtAbort() { return false; }
+
+        Size ScaleToFit(Size from, Size constraint)
+        {
+            double scaleWidth = constraint.Width <= 0 ? 1 : Math.Min(from.Width, constraint.Width) / (double)from.Width;
+            double scaleHeight = constraint.Height <= 0 ? 1 : Math.Min(from.Height, constraint.Height) / (double)from.Height;
+            double scale = Math.Min(scaleWidth, scaleHeight);
+            return new Size((int)Math.Round(from.Width * scale - 0.5), (int)Math.Round(from.Height * scale - 0.5));
+        }
+
+        private void DDSPanel_Resize(object sender, EventArgs e)
+        {
+            if (fit && DateTime.UtcNow > now.AddMilliseconds(25))
+            {
+                pictureBox1.Image = doResize();
+                now = DateTime.UtcNow;
+            }
+            else
+                timer1.Interval = 10;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            pictureBox1.Image = doResize();
+        }
+    }
+}
