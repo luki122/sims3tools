38e54db457616371adca54bd0f0112fcaf8a1e74
pljones <pljones@da88ef80-c9c6-43c1-8b9e-c1dffb33cf72>
2011-05-16 06:49:50 +0000

    Refactor code; rework uvScales handling.

---
+++ trunk/s3pe Helpers/s3pe s3asc Helper/ExportForm.cs
@@ -0,0 +1,129 @@
+﻿/***************************************************************************
+ *  Copyright (C) 2011 by Peter L Jones                                    *
+ *  pljones@users.sf.net                                                   *
+ *                                                                         *
+ *  This file is part of the Sims 3 Package Interface (s3pi)               *
+ *                                                                         *
+ *  s3pi is free software: you can redistribute it and/or modify           *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  s3pi is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with s3pi.  If not, see <http://www.gnu.org/licenses/>.          *
+ ***************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+using s3pi.Interfaces;
+using s3pi.GenericRCOLResource;
+using s3piwrappers;
+
+namespace s3ascHelper
+{
+    public partial class ExportForm : Form, s3pi.Helpers.IRunHelper
+    {
+        public ExportForm()
+        {
+            InitializeComponent();
+            sfdExport.FileName = string.Format("{0}_filebase.s3asc", Program.Filename);
+        }
+
+        Stream stream;
+        public ExportForm(Stream s)
+            : this()
+        {
+            stream = s;
+            Application.DoEvents();
+        }
+
+
+        public byte[] Result { get { return null; } }
+
+        private void Export_Shown(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult dr = sfdExport.ShowDialog();
+                if (dr != DialogResult.OK)
+                {
+                    Environment.ExitCode = 1;
+                    return;
+                }
+
+                string folder = Path.GetDirectoryName(sfdExport.FileName);
+                string filebase = Path.GetFileNameWithoutExtension(sfdExport.FileName).Replace("_filebase", "");
+
+                try
+                {
+                    using (FileStream fs = new FileStream(Path.Combine(folder, string.Format("{0}_filebase.s3asc", filebase)), FileMode.Create, FileAccess.Write))
+                    {
+                        fs.Close();
+                    }
+
+                    GenericRCOLResource rcolResource = new GenericRCOLResource(0, stream);
+                    MLOD mlod = null;
+
+                    if (rcolResource.ChunkEntries[0].TGIBlock.ResourceType == 0x01661233)//MODL
+                    {
+                        this.Text = "Export MODL...";
+                        var lodRef = rcolResource.GetMLODChunkRefforMODL();
+                        if (lodRef == null)
+                        {
+                            CopyableMessageBox.Show("MODL (0x01661233) with no MLOD (0x01D10F34).",
+                                "Invalid MODL resource", CopyableMessageBoxButtons.OK, CopyableMessageBoxIcon.Error);
+                            Environment.ExitCode = 1;
+                            return;
+                        }
+
+
+                        mlod = GenericRCOLResource.ChunkReference.GetBlock(rcolResource, lodRef) as MLOD;
+                    }
+                    else if (rcolResource.ChunkEntries[0].TGIBlock.ResourceType == 0x01D10F34)//MLOD
+                    {
+                        this.Text = "Export MLOD...";
+
+                        mlod = rcolResource.ChunkEntries[0].RCOLBlock as MLOD;
+                    }
+                    else
+                    {
+                        CopyableMessageBox.Show("RCOL resource must be MODL (0x01661233) or MLOD (0x01D10F34).",
+                            "Invalid RCOL resource", CopyableMessageBoxButtons.OK, CopyableMessageBoxIcon.Error);
+                        Environment.ExitCode = 1;
+                        return;
+                    }
+
+                    Export export = new Export(new MyProgressBar(label1, pb));
+
+                    for (int m = 0; m < mlod.Meshes.Count; m++)
+                    {
+                        string fnMesh = Path.Combine(folder, string.Format("{0}_group{1:X2}.s3ascg", filebase, m));
+
+                        using (FileStream fsMesh = new FileStream(fnMesh, FileMode.Create, FileAccess.Write))
+                        {
+                            export.Export_MLOD(new StreamWriter(fsMesh), rcolResource, mlod, mlod.Meshes[m]);
+                            fsMesh.Close();
+                        }
+                    }
+
+                    Environment.ExitCode = 0;
+                }
+                catch (Exception ex)
+                {
+                    CopyableMessageBox.IssueException(ex, "Error");
+                    throw ex;
+                }
+            }
+            finally { this.Close(); }
+        }
+    }
+}
---
+++ trunk/s3pe Helpers/s3pe s3asc Helper/MODL_MLOD_Extensions.cs
@@ -0,0 +1,101 @@
+﻿/***************************************************************************
+ *  Copyright (C) 2011 by Peter L Jones                                    *
+ *  pljones@users.sf.net                                                   *
+ *                                                                         *
+ *  This file is part of the Sims 3 Package Interface (s3pi)               *
+ *                                                                         *
+ *  s3pi is free software: you can redistribute it and/or modify           *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  s3pi is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with s3pi.  If not, see <http://www.gnu.org/licenses/>.          *
+ ***************************************************************************/
+using System;
+using s3pi.Interfaces;
+using s3pi.GenericRCOLResource;
+using s3piwrappers;
+
+namespace s3ascHelper
+{
+    public static class MODL_MLOD_Extensions
+    {
+        public static GenericRCOLResource.ChunkReference GetMLODChunkRefforMODL(this GenericRCOLResource rcolResource)
+        {
+            var lodEntry = (rcolResource.ChunkEntries[0].RCOLBlock as MODL).Entries
+                .Find(l => l.ModelLodIndex.RefType == GenericRCOLResource.ReferenceType.Public
+                    || l.ModelLodIndex.RefType == GenericRCOLResource.ReferenceType.Private);
+            return lodEntry == null ? null : lodEntry.ModelLodIndex;
+        }
+
+        //find the chunk, replace the chunk, perhaps create or remove the reference
+        public static void ReplaceChunk(this GenericRCOLResource rcolResource, MLOD.Mesh mesh, string field, IResourceKey rk, ARCOLBlock block)
+        {
+            ARCOLBlock current = GenericRCOLResource.ChunkReference.GetBlock(rcolResource, (GenericRCOLResource.ChunkReference)mesh[field].Value) as ARCOLBlock;
+            if (block != null)
+            {
+                if (current != null) // replacing is easy
+                {
+                    if (current.Tag != block.Tag)
+                        throw new Exception(string.Format("mesh field {0} is '{1}' but replacement is '{2}'.", field, current.Tag, block.Tag));
+                    // ...not entirely sure if these are required...
+                    current.Data = block.Data;
+                    block = current;
+                }
+                else // adding is okay
+                {
+                    rcolResource.ChunkEntries.Add(rk, block);
+                    mesh[field] = new TypedValue(typeof(GenericRCOLResource.ChunkReference), GenericRCOLResource.ChunkReference.CreateReference(rcolResource, rk), "X");
+                }
+            }
+            else // deleting is not allowed - we can only null the reference, not remove the chunk
+            {
+                mesh[field] = new TypedValue(typeof(GenericRCOLResource.ChunkReference), new GenericRCOLResource.ChunkReference(0, null, 0), "X");
+            }
+        }
+
+        public static float[] GetUVScales(this GenericRCOLResource rcolResource, MLOD.Mesh mesh)
+        {
+            MATD matd = GetMATDforMesh(rcolResource, mesh.MaterialIndex);
+            if (matd != null)
+            {
+                MATD.ShaderData data = (matd.Version < 0x0103 ? matd.Mtrl.SData : matd.Mtnf.SData).Find(x => x.Field == MATD.FieldType.UVScales);
+                if (data != null)
+                {
+                    if (data is MATD.ElementFloat3) { MATD.ElementFloat3 e = data as MATD.ElementFloat3; return new float[] { e.Data0, e.Data1, e.Data2, }; }
+                    else throw new InvalidOperationException(String.Format("Found UVScales of type '{0}'; expected 'ElementFloat3'.", data.GetType().Name));
+                }
+            }
+            return new float[] { 1f / 32767f, };
+        }
+
+        public static void SetUVScales(this GenericRCOLResource rcolResource, MLOD.Mesh mesh, float[] uvScales)
+        {
+            if (uvScales.Length != 3)
+                throw new ArgumentException(String.Format("Found {0} UVScales; expected 3.", uvScales.Length));
+
+            MATD matd = GetMATDforMesh(rcolResource, mesh.MaterialIndex);
+            if (matd != null)
+            {
+                MATD.ShaderDataList sdList = (matd.Version < 0x0103 ? matd.Mtrl.SData : matd.Mtnf.SData);
+                MATD.ShaderData data = sdList.Find(x => x.Field == MATD.FieldType.UVScales);
+                sdList.RemoveAll(x => x.Field == MATD.FieldType.UVScales);
+                sdList.Add(new MATD.ElementFloat3(0, null, MATD.FieldType.UVScales, uvScales[0], uvScales[1], uvScales[2]));
+            }
+        }
+
+        static MATD GetMATDforMesh(GenericRCOLResource rcolResource, GenericRCOLResource.ChunkReference reference)
+        {
+            IRCOLBlock materialRef = GenericRCOLResource.ChunkReference.GetBlock(rcolResource, reference);
+            if (materialRef is MATD) return materialRef as MATD;
+            if (materialRef is MTST) return GetMATDforMesh(rcolResource, (materialRef as MTST).Index);
+            return null;
+        }
+    }
+}
