9c024d56dd4ecd9f3439c3f7b2b008887294aad4
pljones <pljones@da88ef80-c9c6-43c1-8b9e-c1dffb33cf72>
2011-07-12 06:59:20 +0000

    Redesign DDSTool UI; add versioning and assembly information; support import of alpha channel; support use as an s3pe helper.

--- trunk/s3pe Helpers/ViewDDS/TestDDSPanel/Properties/AssemblyInfo.cs
+++ trunk/s3pe Helpers/ViewDDS/TestDDSPanel/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TestDDSPanel")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("s3pe DDS Tool")]
+[assembly: AssemblyDescription("s3pe's own tool for doing things to DDS resources")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("TestDDSPanel")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCompany("Peter L Jones + others")]
+[assembly: AssemblyProduct("s3pe DDS Tool")]
+[assembly: AssemblyCopyright("Copyright © 2011  Peter L Jones  Released under the GPL 3.0")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+//[assembly: AssemblyVersion("1.0.0.0")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
