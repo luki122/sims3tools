9f2b4f55a91b75549637d419fa3c376fdaacfdac
pljones <pljones@da88ef80-c9c6-43c1-8b9e-c1dffb33cf72>
2011-09-29 08:10:28 +0000

    Created for Rockerduck, http://dino.drealm.info/den/denforum/index.php?topic=662.0 - and enhanced to be a proper standalone util.

---
+++ trunk/GetClips/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+using s3pi.Filetable;
+using s3pi.Interfaces;
+using Namemap;
+
+namespace GetClips
+{
+    public partial class Form1 : Form
+    {
+        bool stop = false;
+        bool running = false;
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            GameFoldersForm gff = new GameFoldersForm();
+            gff.ShowInTaskbar = true;
+            gff.StartPosition = FormStartPosition.CenterScreen;
+
+            bool ccEnabled = GetClips.Properties.Settings.Default.CustomContentEnabled;
+            string customContent = GetClips.Properties.Settings.Default.CustomContentPath;
+            string installDirs = GetClips.Properties.Settings.Default.InstallDirs;
+            string epsDisabled = GetClips.Properties.Settings.Default.EPsDisabled;
+
+            for (DialogResult dr = DialogResult.Retry; dr == DialogResult.Retry; )
+            {
+                gff.CCEnabled = ccEnabled;
+                gff.CustomContent = customContent;
+                gff.InstallDirs = installDirs;
+                gff.EPsDisabled = epsDisabled;
+                dr = gff.ShowDialog();
+            }
+            GetClips.Properties.Settings.Default.CustomContentEnabled = FileTable.CustomContentEnabled = gff.CCEnabled;
+            GetClips.Properties.Settings.Default.CustomContentPath = FileTable.CustomContentPath = gff.CustomContent;
+            GetClips.Properties.Settings.Default.InstallDirs = GameFolders.InstallDirs = gff.InstallDirs;
+            GetClips.Properties.Settings.Default.EPsDisabled = epsDisabled;
+            GetClips.Properties.Settings.Default.Save();
+
+            FileTable.FileTableEnabled = true;
+
+            NameMap.Reset();
+        }
+
+        static int updateEvery = 1000;
+        static int tickTime = 50;
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            if (FileTable.GameContent == null)
+            {
+                richTextBox1.Text = "No game content found.";
+                return;
+            }
+
+            running = true;
+
+            List<SpecificIndexEntry> clips = new List<SpecificIndexEntry>();
+
+            DateTime now = DateTime.UtcNow.AddMilliseconds(updateEvery);
+            DateTime tick = DateTime.UtcNow.AddMilliseconds(tickTime);
+            foreach (var sie in FileTable.GameContent.SelectMany(ppt => ppt.Package.FindAll(rie => rie.ResourceType == 0x6B20C4F3).Select(rie => new SpecificIndexEntry(ppt, rie))))
+            {
+                if (stop) { Environment.Exit(0); return; }
+                if (DateTime.UtcNow > now)
+                {
+                    if (richTextBox1.Text.Length > 3)
+                        richTextBox1.Text = "...";
+                    else
+                        richTextBox1.Text = "Please wait, processing... " + clips.Count + "\n" + sie.PathPackage.Path;
+                    Application.DoEvents();
+                    now = DateTime.UtcNow.AddMilliseconds(updateEvery);
+                }
+                if (DateTime.UtcNow > tick)
+                {
+                    Application.DoEvents();
+                    tick = DateTime.UtcNow.AddMilliseconds(tickTime);
+                }
+                if (clips.Exists(x => x.RequestedRK.Instance == sie.RequestedRK.Instance)) continue;
+                clips.Add(sie);
+            }
+
+            richTextBox1.Text = "Please wait, sorting output...";
+            Application.DoEvents();
+            clips.Sort((x, y) => (x.RequestedRK.Instance << 8).CompareTo(y.RequestedRK.Instance << 8));
+
+            richTextBox1.Text = "Please wait, formatting output...";
+            Application.DoEvents();
+            string[] lines = new string[clips.Count];
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (stop) { Environment.Exit(0); return; }
+                if (DateTime.UtcNow > tick)
+                {
+                    Application.DoEvents();
+                    tick = DateTime.UtcNow.AddMilliseconds(tickTime);
+                }
+
+                string name = NameMap.NMap[clips[i].RequestedRK.Instance];
+                lines[i] = (name == null ? "(unknown)" : name) + "  " + clips[i].RequestedRK + "  " + clips[i].PathPackage.Path;
+            }
+
+            if (stop) { Environment.Exit(0); return; }
+            richTextBox1.Lines = lines;
+            running = false;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing && running)
+            {
+                stop = true;
+                e.Cancel = true;
+            }
+        }
+    }
+}
---
+++ trunk/GetClips/NameMap.cs
@@ -0,0 +1,72 @@
+﻿/***************************************************************************
+ *  Copyright (C) 2011 by Peter L Jones                                    *
+ *  pljones@users.sf.net                                                   *
+ *                                                                         *
+ *  This file is part of the Sims 3 Package Interface (s3pi)               *
+ *                                                                         *
+ *  s3pi is free software: you can redistribute it and/or modify           *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  s3pi is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with s3pi.  If not, see <http://www.gnu.org/licenses/>.          *
+ ***************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using s3pi.Interfaces;
+using s3pi.Filetable;
+
+namespace Namemap
+{
+    public class NameMap
+    {
+        SpecificResource latest = null;
+        List<IDictionary<ulong, string>> namemaps;
+        public NameMap(List<PathPackageTuple> nameMapPPTs)
+        {
+            namemaps = new List<IDictionary<ulong, string>>();
+            if (nameMapPPTs == null) return;
+            foreach (var sr in nameMapPPTs.SelectMany(ppt => ppt.FindAll(rie => rie.ResourceType == 0x0166038C)))
+            {
+                IDictionary<ulong, string> nmp = sr.Resource as IDictionary<ulong, string>;
+                if (nmp == null) continue;
+                if (latest == null) latest = sr;
+                namemaps.Add(nmp);
+            }
+        }
+        public string this[ulong instance]
+        {
+            get
+            {
+                foreach (IDictionary<ulong, string> namemap in namemaps)
+                    if (namemap.ContainsKey(instance))
+                        return namemap[instance];
+                return null;
+            }
+        }
+        public IResourceKey rk { get { return latest == null ? RK.NULL : latest.RequestedRK; } }
+        public IDictionary<ulong, string> map { get { return latest == null ? null : (IDictionary<ulong, string>)latest.Resource; } }
+        public void Commit() { latest.Commit(); }
+
+        static NameMap nmap;
+        public static void Reset() { nmap = null; }
+        public static NameMap NMap
+        {
+            get
+            {
+                if (nmap == null)
+                    nmap = new NameMap(FileTable.GameContent);
+                return nmap;
+            }
+        }
+        public static bool IsOK { get { return NMap != null && NMap.map != null && NMap.map.Count > 0; } }
+    }
+}
