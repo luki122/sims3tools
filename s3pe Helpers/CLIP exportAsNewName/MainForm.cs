4d5eadb873404424d4e69bac8ec788886e803243
pljones <pljones@da88ef80-c9c6-43c1-8b9e-c1dffb33cf72>
2012-02-16 22:02:05 +0000

    Use the library FNV64CLIP to get the IID.

--- s3pe Helpers/CLIP exportAsNewName/MainForm.cs
+++ s3pe Helpers/CLIP exportAsNewName/MainForm.cs
@@ -21,8 +21,9 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
-using System.Windows.Forms;
 using System.IO;
+using System.Security.Cryptography;
+using System.Windows.Forms;
 using s3pi.Helpers;
 
 namespace CLIPexportAsNewName
@@ -41,7 +42,7 @@ namespace CLIPexportAsNewName
 
         public byte[] Result { get { return null; } }
 
-        private void tbClipName_TextChanged(object sender, EventArgs e) { tbIID.Text = string.Format("0x{0:X16}", IID(tbClipName.Text)); }
+        private void tbClipName_TextChanged(object sender, EventArgs e) { tbIID.Text = string.Format("0x{0:X16}", FNV64CLIP.GetHash(tbClipName.Text)); }
 
         private void btnCancel_Click(object sender, EventArgs e) { this.Close(); }
 
@@ -57,7 +58,7 @@ namespace CLIPexportAsNewName
                 CLIPexportAsNewName.Properties.Settings.Default.Save();
 
                 string filename = Path.Combine(folderBrowserDialog1.SelectedPath, String.Format("S3_{0:X8}_{1:X8}_{2:X16}_{3}%%+CLIP.animation",
-                    0x6B20C4F3, 0, IID(tbClipName.Text), tbClipName.Text));
+                    0x6B20C4F3, 0, FNV64CLIP.GetHash(tbClipName.Text), tbClipName.Text));
 
                 if (File.Exists(filename) && CopyableMessageBox.Show(String.Format("File '{0}' exists.\n\nReplace?", Path.GetFileName(filename)), "File exists",
                          CopyableMessageBoxButtons.YesNo, CopyableMessageBoxIcon.Question, 1, 1) != 0) return;
@@ -73,56 +74,5 @@ namespace CLIPexportAsNewName
             finally { this.Close(); }
         }
 
-        static char[] ao = new char[] { 'a', 'o', };
-        ulong IID(string text)
-        {
-            ulong mask = 0;
-
-            string[] split = tbClipName.Text.Split(new char[] { '_', }, 2);
-
-            string value = tbClipName.Text;
-            if (split.Length > 1)
-            {
-                if (split[0].Length == 1)
-                {
-                    if (ao.Contains<char>(split[0][0])) { }
-                    else { value = string.Join("_", new string[] { "a", split[1], }); mask = (ulong)(0x8000 | Mask(split[0][0]) << 8); }
-                }
-                else if (split[0].Length == 3 && split[0][1] == '2')
-                {
-                    if (ao.Contains<char>(split[0][0]) && ao.Contains<char>(split[0][2])) { }
-                    else
-                    {
-                        value = string.Join("_", new string[] { (split[0][0] == 'o' ? "o" : "a") + "2" + (split[0][2] == 'o' ? "o" : "a"), split[1], });
-                        mask = (ulong)(0x8000 | Mask(split[0][0]) << 8 | Mask(split[0][2]));
-                    }
-                }
-            }
-
-            ulong iid = System.Security.Cryptography.FNV64.GetHash(value);
-            iid &= 0x7FFFFFFFFFFFFFFF;
-            iid ^= mask << 48;
-
-            return iid;
-        }
-
-        byte Mask(char age)
-        {
-            switch (age)
-            {
-                case 'b': return 0x01;
-                case 'p': return 0x02;
-                case 'c': return 0x03;
-                case 't': return 0x04;
-                case 'h': return 0x05;
-                case 'e': return 0x06;
-                default: return 0x00;
-            }
-        }
-    }
-
-    static class Extensions
-    {
-        public static bool Contains<T>(this IEnumerable<T> haystack, T needle) { foreach (var x in haystack) if (needle.Equals(x)) return true; return false; }
     }
 }
