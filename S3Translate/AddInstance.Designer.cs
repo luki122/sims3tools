bd96cb0b42b42d270d720029bfd6f21bbfedc23d
pljones <pljones@da88ef80-c9c6-43c1-8b9e-c1dffb33cf72>
2013-04-30 10:39:55 +0000

    Change name of AddInstance to AddEditGUID, as that's what it does and change its Instance property to GUID.

--- S3Translate/AddInstance.Designer.cs
+++ S3Translate/AddInstance.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace S3Translate
 {
-    partial class AddInstance
+    partial class AddEditGUID
     {
         /// <summary>
         /// Required designer variable.
@@ -52,28 +52,15 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 2);
             this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 0, 1);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(16, 15);
-            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowCount = 3;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(780, 154);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(585, 101);
             this.tableLayoutPanel1.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Right;
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(50, 9);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(90, 17);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Text to hash:";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // tableLayoutPanel2
             // 
@@ -85,21 +72,20 @@
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel2.Controls.Add(this.btnCancel, 0, 0);
             this.tableLayoutPanel2.Controls.Add(this.btnOK, 1, 0);
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(564, 98);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(423, 72);
             this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(0);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 1;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(216, 36);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(162, 29);
             this.tableLayoutPanel2.TabIndex = 2;
             // 
             // btnCancel
             // 
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancel.Location = new System.Drawing.Point(4, 4);
-            this.btnCancel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCancel.Location = new System.Drawing.Point(3, 3);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(100, 28);
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
             this.btnCancel.TabIndex = 5;
             this.btnCancel.Text = "C&ancel";
             this.btnCancel.UseVisualStyleBackColor = true;
@@ -107,10 +93,9 @@
             // btnOK
             // 
             this.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.btnOK.Location = new System.Drawing.Point(112, 4);
-            this.btnOK.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnOK.Location = new System.Drawing.Point(84, 3);
             this.btnOK.Name = "btnOK";
-            this.btnOK.Size = new System.Drawing.Size(100, 28);
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
             this.btnOK.TabIndex = 4;
             this.btnOK.Text = "&OK";
             this.btnOK.UseVisualStyleBackColor = true;
@@ -121,7 +106,7 @@
             this.tableLayoutPanel3.AutoSize = true;
             this.tableLayoutPanel3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.tableLayoutPanel3.ColumnCount = 3;
-            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 144F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
             this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
             this.tableLayoutPanel3.Controls.Add(this.label1, 0, 0);
@@ -129,71 +114,80 @@
             this.tableLayoutPanel3.Controls.Add(this.tbInstance, 1, 1);
             this.tableLayoutPanel3.Controls.Add(this.label2, 0, 1);
             this.tableLayoutPanel3.Controls.Add(this.btnFNV, 2, 1);
-            this.tableLayoutPanel3.Location = new System.Drawing.Point(4, 4);
-            this.tableLayoutPanel3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 3);
             this.tableLayoutPanel3.Name = "tableLayoutPanel3";
             this.tableLayoutPanel3.RowCount = 2;
-            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel3.Size = new System.Drawing.Size(772, 72);
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(579, 55);
             this.tableLayoutPanel3.TabIndex = 3;
             // 
-            // tbInstance
+            // label1
             // 
-            this.tbInstance.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.tbInstance.Location = new System.Drawing.Point(148, 43);
-            this.tbInstance.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.tbInstance.Name = "tbInstance";
-            this.tbInstance.Size = new System.Drawing.Size(512, 22);
-            this.tbInstance.TabIndex = 2;
-            this.tbInstance.Text = "WxWWWWWWWWWWWWWWWW";
-            this.tbInstance.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
-            // 
-            // btnFNV
-            // 
-            this.btnFNV.Location = new System.Drawing.Point(668, 40);
-            this.btnFNV.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.btnFNV.Name = "btnFNV";
-            this.btnFNV.Size = new System.Drawing.Size(100, 28);
-            this.btnFNV.TabIndex = 3;
-            this.btnFNV.Text = "&Generate";
-            this.btnFNV.UseVisualStyleBackColor = true;
-            this.btnFNV.Click += new System.EventHandler(this.btnFNV_Click);
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Text to hash:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // textBox_StringToHash
             // 
             this.textBox_StringToHash.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.tableLayoutPanel3.SetColumnSpan(this.textBox_StringToHash, 2);
-            this.textBox_StringToHash.Location = new System.Drawing.Point(148, 7);
-            this.textBox_StringToHash.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_StringToHash.Location = new System.Drawing.Point(96, 3);
             this.textBox_StringToHash.Name = "textBox_StringToHash";
-            this.textBox_StringToHash.Size = new System.Drawing.Size(620, 22);
+            this.textBox_StringToHash.Size = new System.Drawing.Size(480, 20);
             this.textBox_StringToHash.TabIndex = 4;
             this.textBox_StringToHash.Text = "WxWWWWWWWWWWWWWWWW";
+            // 
+            // tbInstance
+            // 
+            this.tbInstance.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbInstance.Location = new System.Drawing.Point(96, 30);
+            this.tbInstance.Name = "tbInstance";
+            this.tbInstance.Size = new System.Drawing.Size(399, 20);
+            this.tbInstance.TabIndex = 2;
+            this.tbInstance.Text = "WxWWWWWWWWWWWWWWWW";
+            this.tbInstance.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // label2
             // 
             this.label2.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(24, 45);
+            this.label2.Location = new System.Drawing.Point(2, 34);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(117, 17);
+            this.label2.Size = new System.Drawing.Size(89, 13);
             this.label2.TabIndex = 5;
             this.label2.Text = "GUID as number:";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
-            // AddInstance
+            // btnFNV
+            // 
+            this.btnFNV.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnFNV.Location = new System.Drawing.Point(501, 29);
+            this.btnFNV.Name = "btnFNV";
+            this.btnFNV.Size = new System.Drawing.Size(75, 23);
+            this.btnFNV.TabIndex = 3;
+            this.btnFNV.Text = "&Generate";
+            this.btnFNV.UseVisualStyleBackColor = true;
+            this.btnFNV.Click += new System.EventHandler(this.btnFNV_Click);
+            // 
+            // AddEditGUID
             // 
             this.AcceptButton = this.btnOK;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnCancel;
-            this.ClientSize = new System.Drawing.Size(812, 184);
+            this.ClientSize = new System.Drawing.Size(609, 126);
             this.Controls.Add(this.tableLayoutPanel1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.Name = "AddInstance";
+            this.MinimumSize = new System.Drawing.Size(617, 145);
+            this.Name = "AddEditGUID";
             this.Text = "Add GUID";
-            this.Shown += new System.EventHandler(this.AddGUID_Shown);
+            this.Shown += new System.EventHandler(this.AddEditGUID_Shown);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
             this.tableLayoutPanel2.ResumeLayout(false);
