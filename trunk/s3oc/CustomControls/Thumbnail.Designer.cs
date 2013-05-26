b631962ac58f2c12fb167a0420f087cd5e4eaa17
pljones <pljones@da88ef80-c9c6-43c1-8b9e-c1dffb33cf72>
2013-05-26 19:46:44 +0000

    Prevent font-bound scaling on Thumbnail control.

--- trunk/s3oc/CustomControls/Thumbnail.Designer.cs
+++ trunk/s3oc/CustomControls/Thumbnail.Designer.cs
@@ -154,8 +154,7 @@
             // 
             // Thumbnail
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Thumbnail";
             this.Size = new System.Drawing.Size(381, 128);
